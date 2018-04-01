bb790fcae0c16c1a95c0dd3b9bf791850d96af14
“soczynskiT <t.soczynski86@gmail.com>
2018-04-01 15:34:20 +0200

    Module 13.1

--- kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
+++ kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
@@ -12,7 +12,7 @@ public class DbManager {
     private DbManager() throws SQLException {
         Properties connectionProps = new Properties();
         connectionProps.put("user", "kodilla_user");
-        connectionProps.put("password", "Soczynski1986MySQL");
+        connectionProps.put("password", "kodilla_password");
         conn = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/kodilla_course?serverTimezone=Europe/Warsaw" +
                         "&useSSL=False",
